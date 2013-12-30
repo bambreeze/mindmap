e4e042be51442b9f1ad715a6fba1c89708a00acb
bambreeze <bambreeze@gmail.com>
2013-12-30 15:14:56 +0800

    add infrastructure of Linux pci driver

---
+++ Linux/device_driver_pci.mm
@@ -0,0 +1,37 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1388324069364" ID="ID_1067822762" MODIFIED="1388324200527" TEXT="pci subsystem">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1388324202817" ID="ID_837080186" MODIFIED="1388387575875" POSITION="right" TEXT="pci overview">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1388382046488" ID="ID_1731680601" MODIFIED="1388387004844" TEXT="PCI (Peripheral Component Interconnect), is a local computer bus for attaching hardware devices in a computer."/>
+<node CREATED="1388382100102" ID="ID_595199588" MODIFIED="1388385333820">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="driver/pci/pci-config-space.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1388382109422" ID="ID_883886368" MODIFIED="1388382112714" TEXT="reference">
+<node CREATED="1388382118958" ID="ID_1207389878" MODIFIED="1388382171790" TEXT="http://www.pcisig.com/home"/>
+<node CREATED="1388382173548" ID="ID_428779289" MODIFIED="1388382186342" TEXT="http://en.wikipedia.org/wiki/PCI_configuration_space"/>
+<node CREATED="1388386113329" ID="ID_266613301" MODIFIED="1388386440803" TEXT="http://mj.ucw.cz/sw/pciutils/"/>
+</node>
+</node>
+<node CREATED="1388324207605" ID="ID_1599119370" MODIFIED="1388324231417" POSITION="right" TEXT="pci driver framework">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1388387066522" ID="ID_834237932" MODIFIED="1388387351229" TEXT="drivers/pci/pci.c">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="attach"/>
+</node>
+<node CREATED="1388387361332" ID="ID_1654791867" MODIFIED="1388387361332" TEXT=""/>
+</node>
+<node CREATED="1388324213789" ID="ID_1002496453" MODIFIED="1388324228931" POSITION="right" TEXT="pci example">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
