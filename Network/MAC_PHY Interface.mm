1e94d5226e1b48caca8e5da6d8ba455134e64c13
bambreeze <bambreeze@gmail.com>
2014-01-08 11:34:22 +0800

    restore missing changed caused by *git reset --hard commit_id*

---
+++ Network/MAC_PHY Interface.mm
@@ -0,0 +1,24 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1388630671859" ID="ID_654822713" MODIFIED="1388630776477" TEXT="MAC/PHY Interface">
+<node CREATED="1388631047779" ID="ID_624370409" MODIFIED="1388631198887" POSITION="right" TEXT="XAUI/XLAUI&#x5728;&#x7269;&#x7406;&#x7ed3;&#x6784;&#x4e0a;&#x662f;&#x4e00;&#x6837;&#x7684;&#xff0c;&#x6536;&#x53d1;&#x901a;&#x9053;&#x72ec;&#x7acb;&#xff0c;&#x5404;&#x56db;&#x5bf9;&#x5dee;&#x5206;&#x4fe1;&#x53f7;&#x7ebf;&#xff08;&#x4e32;&#x884c;&#x603b;&#x7ebf;&#xff09;&#xff09;&#x3002;"/>
+<node CREATED="1388631009523" ID="ID_1078009939" MODIFIED="1388631022313" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="XAUI_XLAUI_CAUI%20Interface.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1388630699836" ID="ID_807344228" MODIFIED="1388631103707" POSITION="right" TEXT="XAUI - 10GE">
+<node CREATED="1388631069546" ID="ID_731318464" MODIFIED="1388631073080" TEXT="&#x5bf9;&#x4e8e;XAUI&#x603b;&#x7ebf;&#xff0c;&#x6bcf;&#x5bf9;&#x5dee;&#x5206;&#x7ebf;&#x4e0a;&#x7684;&#x6570;&#x636e;&#x901f;&#x7387;&#x4e3a;3.125Gbps&#xff0c;&#x603b;&#x6570;&#x636e;&#x5e26;&#x5bbd;&#x4e3a;12.5Gbps&#xff0c;&#x6709;&#x6548;&#x5e26;&#x5bbd;&#x4e3a;12.5Gbps*0.8=10Gbps &#xff08;&#x56e0;&#x4e3a;XAUI&#x603b;&#x7ebf;&#x6570;&#x636e;&#x5728;&#x4f20;&#x8f93;&#x524d;&#x8fdb;&#x884c;&#x4e86;8B/10B&#x53d8;&#x6362;&#xff0c;&#x7f16;&#x7801;&#x6548;&#x7387;&#x4e3a;80%&#xff09;&#x3002;"/>
+</node>
+<node CREATED="1388630704075" ID="ID_675482302" MODIFIED="1388631103699" POSITION="right" TEXT="XLAUI - 40GE">
+<node CREATED="1388631081866" ID="ID_1294435612" MODIFIED="1388631084216" TEXT="&#x5bf9;&#x4e8e;XLAUI&#x603b;&#x7ebf;&#xff0c;&#x6bcf;&#x5bf9;&#x5dee;&#x5206;&#x7ebf;&#x4e0a;&#x7684;&#x6570;&#x636e;&#x901f;&#x7387;&#x4e3a;10.3125Gbps&#xff0c;&#x603b;&#x6570;&#x636e;&#x5e26;&#x5bbd;&#x4e3a;41.25Gbps&#xff0c;&#x6709;&#x6548;&#x5e26;&#x5bbd;&#x4e3a;41.25Gbps*(64/66)=40Gbps&#xff08;&#x56e0;&#x4e3a;XLAUI&#x603b;&#x7ebf;&#x6570;&#x636e;&#x5728;&#x4f20;&#x8f93;&#x524d;&#x8fdb;&#x884c;&#x4e86;64B/66B&#x53d8;&#x6362;&#xff0c;&#x7f16;&#x7801;&#x6548;&#x7387;&#x4e3a;96.97%&#xff09;&#x3002;"/>
+</node>
+<node CREATED="1388630707474" ID="ID_560509924" MODIFIED="1388631103692" POSITION="right" TEXT="CAUI - 100GE"/>
+</node>
+</map>
