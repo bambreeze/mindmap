07633deb522fe427f1a64ad8f8117022f82b723b
bambreeze <bambreeze@gmail.com>
2014-03-21 14:12:03 +0800

    add professional skills

---
+++ Career/professinonal.mm
@@ -0,0 +1,42 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1395380979258" ID="ID_636144506" MODIFIED="1395382021543" TEXT="professinonal">
+<node CREATED="1395381693777" ID="ID_1807903941" MODIFIED="1395382240479" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Concept/Product&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;---&#160;&#160;&#160;&#160;Market skills
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      =========== Product Manager ==========
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Design&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;---&#160;&#160;&#160;&#160;Business &amp; Technical skills
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ============ Architecture ============
+    </p>
+    <p>
+      &#160;
+    </p>
+    <p>
+      Implementation&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;---&#160;&#160;&#160;&#160;Technical skills
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
