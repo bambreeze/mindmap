5c8f3e3acdd43aa50af67f3b4127b44fbcf29c04
bambreeze <bambreeze@gmail.com>
2015-01-12 10:40:11 +0800

    update by notes from book 走出软件作坊

--- Career/professinonal.mm
+++ Career/professinonal.mm
@@ -1,7 +1,14 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1395380979258" ID="ID_636144506" MODIFIED="1395382021543" TEXT="professinonal">
-<node CREATED="1395381693777" ID="ID_1807903941" MODIFIED="1395382240479" POSITION="right">
+<node CREATED="1421030180364" ID="ID_1014102886" MODIFIED="1421030190160" POSITION="right" TEXT="&#x505a;&#x4eba;">
+<node CREATED="1421030193736" ID="ID_948745524" MODIFIED="1421030214661" TEXT="&#x4e2a;&#x4eba;&#x7d20;&#x8d28;"/>
+<node CREATED="1421030215599" ID="ID_1005050848" MODIFIED="1421030219571" TEXT="&#x4eba;&#x9645;&#x5173;&#x7cfb;"/>
+</node>
+<node CREATED="1421030190552" ID="ID_444819347" MODIFIED="1421030192475" POSITION="right" TEXT="&#x505a;&#x4e8b;">
+<node CREATED="1421030221423" ID="ID_1055835993" MODIFIED="1421030225012" TEXT="&#x4e1a;&#x52a1;"/>
+<node CREATED="1421030225407" ID="ID_326585199" MODIFIED="1421030227388" TEXT="&#x6280;&#x672f;"/>
+<node CREATED="1395381693777" ID="ID_1807903941" MODIFIED="1395382240479">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -35,8 +42,8 @@
       Implementation&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;---&#160;&#160;&#160;&#160;Technical skills
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
+</node>
 </node>
 </node>
 </map>
