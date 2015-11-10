bf8001a854e2e120881a09fe74c343d580cac410
bambreeze <bambreeze@gmail.com>
2015-11-10 17:34:41 +0800

    add refactoring mindmapping

---
+++ Career/refactoring.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1447147507500" ID="ID_1141498859" MODIFIED="1447147576928" TEXT="&#x91cd;&#x6784;(Refactoring)">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1447147843060" ID="ID_1039884777" MODIFIED="1447147900466" POSITION="right" TEXT="&#x91cd;&#x6784;&#xff0c;&#x662f;&#x5728;&#x4e0d;&#x6539;&#x53d8;&#x8f6f;&#x4ef6;&#x53ef;&#x89c2;&#x5bdf;&#x884c;&#x4e3a;&#x7684;&#x524d;&#x63d0;&#x4e0b;&#xff0c;&#x6539;&#x5584;&#x5176;&#x5185;&#x90e8;&#x7ed3;&#x6784;"/>
+<node CREATED="1447147901813" ID="ID_1027737904" MODIFIED="1447147946552" POSITION="right" TEXT="&#x7b2c;&#x4e00;&#x6b65;&#xff0c;&#x6784;&#x5efa;&#x53ef;&#x9760;&#x7684;&#x6d4b;&#x8bd5;&#x73af;&#x5883;"/>
+<node CREATED="1447147831776" ID="ID_1169954926" MODIFIED="1447147836689" POSITION="right" TEXT="&#x611f;&#x609f;">
+<node CREATED="1447147618809" ID="ID_1403587610" MODIFIED="1447147632509" TEXT="&#x6a21;&#x5f0f;&#x662f;&#x8bbe;&#x8ba1;&#x7684;&#x5f00;&#x59cb;&#xff0c;&#x8fc7;&#x7a0b;&#x4e2d;&#x53ef;&#x4ee5;&#x91c7;&#x7528;&#x91cd;&#x6784;&#x4e0d;&#x65ad;&#x4f18;&#x5316;&#x8bbe;&#x8ba1;&#x3002;"/>
+<node CREATED="1447147596339" ID="ID_1805218336" MODIFIED="1447147688486" TEXT="&#x53ef;&#x4ee5;&#x91c7;&#x7528;&#x201c;&#x7ed3;&#x5bf9;&#x7f16;&#x7a0b;&#x201d;&#x7684;&#x65b9;&#x5f0f;"/>
+<node CREATED="1447147633217" ID="ID_526334301" MODIFIED="1447147696900" TEXT="&#x9700;&#x8981;Manager&#x7684;&#x652f;&#x6301;">
+<node CREATED="1447147643993" ID="ID_712888344" MODIFIED="1447147701594" TEXT="&#x4e89;&#x53d6;&#x66f4;&#x591a;&#x7684;&#x65f6;&#x95f4;(deadline)">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1447147660544" ID="ID_716297896" MODIFIED="1447147705778" TEXT="&#x4e89;&#x53d6;&#x66f4;&#x591a;&#x7684;&#x8d44;&#x6e90;(Push more Engineer join in)">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+</node>
+</node>
+</map>
